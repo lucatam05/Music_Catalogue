cb43be2ca695e25e6302bf5df76dfb9e28373d3b
lucatam05 <tamiazzoluca2912@gmail.com>
2026-07-06 15:57:47 +0200

    update program.cs

--- Music.Catalogue.WebApi/Program.cs
+++ Music.Catalogue.WebApi/Program.cs
@@ -16,10 +16,7 @@ builder.Services.AddDbContext<CatalogueDbContext>(options =>
 builder.Services.AddScoped<IBusiness, Business>();
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddHttpClient<ISpotifyClient, SpotifyClient>();
-builder.Services.AddHttpClient<IClientHttp, ClientHttp>("CatalogueClient", client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["Services:Catalogue"]!);
-});
+builder.Services.AddHttpClient<IClientHttp, ClientHttp>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
